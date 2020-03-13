c23a5e3e7b4768e6a2dca904b2842ca4fb11714a
Eric Stanley <eric.stanleyjones@gmail.com>
2020-03-13 15:34:48 +0530

    Parameterized api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 declare var process: { env: { [key: string]: string; } };
 export const environment = {
   production: true,
-  apiURL: 'https://heroku-mean-demo-backend.herokuapp.com/api/'
+  apiURL: process.env.API_URL
 };
