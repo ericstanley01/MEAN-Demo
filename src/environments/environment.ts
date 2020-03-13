793f48afc099af447b1e4bfc6385c5283a3f71f3
Eric Stanley <eric.stanleyjones@gmail.com>
2020-03-13 15:27:13 +0530

    Added process variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+declare var process: { env: { [key: string]: string; } };
 export const environment = {
   production: false,
   apiURL: process.env.API_URL
