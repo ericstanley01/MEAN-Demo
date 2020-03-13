42d68192c8d3f3a3eccaa2970603348fa718bedc
Eric Stanley <eric.stanleyjones@gmail.com>
2020-03-13 15:31:05 +0530

    Testing with actual api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 declare var process: { env: { [key: string]: string; } };
 export const environment = {
   production: true,
-  apiURL: process.env.API_URL
+  apiURL: 'https://heroku-mean-demo-backend.herokuapp.com/api/'
 };
