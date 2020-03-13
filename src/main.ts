0d5af2a249e829e3f6e841ea747ec8dc874c3a7a
Eric Stanley <eric.stanleyjones@gmail.com>
2020-03-13 15:46:39 +0530

    Removed console.log

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
-  console.log(environment.apiURL);
   enableProdMode();
 }
 
