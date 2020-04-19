e51e7c4536c448dc1bf1513c38408fe155254eca
Eric Stanley <eric.stanleyjones@gmail.com>
2020-04-20 00:51:37 +0530

    Delete environment.prod.ts

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-// require("dotenv").config();
-declare var process: { env: { [key: string]: string } };
-export const environment = {
-  production: true,
-  apiURL: "https://heroku-mean-demo-backend.herokuapp.com/api",
-};
